d59b92d17582d2432bd7c795253d0ef701d451fd
noriwaka <airsoft.manatsu@gmail.com>
2024-04-28 15:04:34 +0900

    classBase

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,4 +1,7 @@
 <x-tests.app>
 <x-slot name="header">ヘッダー2</x-slot>
 component-test2
-</x-tests.app>
+<x-test-class-base classBaseMessage="メッセージです" />
+<div class="mb-4"></div>
+<x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更しています" />
+</x-test.app>
